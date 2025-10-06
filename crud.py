8798bc7468cfc77f5298eb521a06f3c51b66e4c4
Alesandra <alesandra.a.silva@aluno.senai.br>
2025-10-06 14:47:21 -0300

    função deletar aluno✔

--- crud.py
+++ crud.py
@@ -39,6 +39,20 @@ def atualizar_idade(id_aluno, nova_idade):
         except Exception as erro:
                 print(f"Erro ao tentar listar alunos: {erro}")
         finally:
-                cursor.close()
-                conexao.close()
+            cursor.close()
+            conexao.close()
+
+
+def deletar_aluno(id_aluno):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute("DELETE FROM alunos WHERE id = %s" ,
+            (id_aluno,))
+            conexao.commit()
+        except Exception as erro:
+                print(f"Erro ao tentar deletar aluno: {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
 
